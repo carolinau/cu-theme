674e41f67da0b3e3c997fb5f1514a9d81722d020
Jordan Graham <jordan@atendesigngroup.com>
2024-01-04 15:44:01 -0700

    CARU-7: Updated settings.php to allow for a custom pantheon settings file.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -9,12 +9,29 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  * Include the Pantheon-specific settings file.
  *
  * n.b. The settings.pantheon.php file makes some changes
- *      that affect all envrionments that this site
+ *      that affect all environments that this site
  *      exists in.  Always include this file, even in
- *      a local development environment, to insure that
+ *      a local development environment, to ensure that
  *      the site settings remain consistent.
  */
 include __DIR__ . "/settings.pantheon.php";
+
+/**
+ * Skipping permissions hardening will make scaffolding
+ * work better, but will also raise a warning when you
+ * install Drupal.
+ *
+ * https://www.drupal.org/project/drupal/issues/3091285
+ */
+// $settings['skip_permissions_hardening'] = TRUE;
+
+/**
+ * Include the custom pantheon settings file if it exists.
+ */
+$custom_pantheon_settings = __DIR__ . "/settings.pantheon-custom.php";
+if (file_exists($custom_pantheon_settings)) {
+  include $custom_pantheon_settings;
+}
 
 /**
  * If there is a local settings file, then include it
